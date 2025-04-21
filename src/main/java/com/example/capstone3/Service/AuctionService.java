1b0eed4635e97a20bedcef0d2dc05eca7a15a847
Khalidsdhan <khalid@example.com>
2025-04-21 19:28:00 +0300

    add

--- src/main/java/com/example/capstone3/Service/AuctionService.java
+++ src/main/java/com/example/capstone3/Service/AuctionService.java
@@ -1,9 +1,50 @@
 package com.example.capstone3.Service;
 
+import com.example.capstone3.Api.ApiException;
+import com.example.capstone3.Model.Auction;
+import com.example.capstone3.Repository.AuctionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class AuctionService {
+
+    private final AuctionRepository auctionRepository;
+
+
+
+    public List<Auction> getAllAuction(){
+        return auctionRepository.findAll();
+    }
+
+
+    public void addAuction(Auction auction){
+        auctionRepository.save(auction);
+    }
+
+
+    public void updateAuction(Integer id,Auction auction){
+        Auction oldAuction=auctionRepository.findAuctionsById(id);
+        if(oldAuction==null){
+            throw new ApiException("Auction is not found");
+        }
+        oldAuction.setStartTime(auction.getStartTime());
+        oldAuction.setEndTime(auction.getEndTime());
+        oldAuction.setIsActive(auction.getIsActive());
+
+        auctionRepository.save(oldAuction);
+    }
+
+
+    public void deleteAuction(Integer id){
+        Auction auction=auctionRepository.findAuctionsById(id);
+
+        if(auction==null){
+            throw new ApiException("Auction is not found");
+        }
+        auctionRepository.delete(auction);
+    }
 }
--- src/main/java/com/example/capstone3/Service/CustomerService.java
+++ src/main/java/com/example/capstone3/Service/CustomerService.java
@@ -1,9 +1,50 @@
 package com.example.capstone3.Service;
 
+import com.example.capstone3.Api.ApiException;
+import com.example.capstone3.Model.Customer;
+import com.example.capstone3.Repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+
+    public List<Customer> getAllCustomer(){
+        return customerRepository.findAll();
+    }
+
+
+    public void addCustomer(Customer customer){
+        customerRepository.save(customer);
+    }
+
+
+    public void updateCustomer(Integer id,Customer customer){
+        Customer oldCustomer=customerRepository.findCustomerById(id);
+        if(customer==null){
+            throw new ApiException("customer is not found");
+        }
+        oldCustomer.setName(customer.getName());
+        oldCustomer.setAge(customer.getAge());
+        oldCustomer.setEmail(customer.getEmail());
+        oldCustomer.setPassword(customer.getPassword());
+
+        customerRepository.save(oldCustomer);
+    }
+
+
+    public void deleteCustomer(Integer id){
+        Customer customer=customerRepository.findCustomerById(id);
+
+        if(customer==null){
+            throw new ApiException("customer is not found");
+        }
+        customerRepository.delete(customer);
+    }
 }
