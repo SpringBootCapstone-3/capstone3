88e6ef9b38b437f32dd467d05241ed76ff0dc96a
AbdullahAli1999 <iifamxx@gmail.com>
2025-04-21 19:51:21 +0300

    i did crud for property in service

--- src/main/java/com/example/capstone3/Service/PropertyService.java
+++ src/main/java/com/example/capstone3/Service/PropertyService.java
@@ -1,5 +1,6 @@
 package com.example.capstone3.Service;
 
+import com.example.capstone3.Api.ApiException;
 import com.example.capstone3.Model.Property;
 import com.example.capstone3.Repository.PropertyRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,4 +21,26 @@ public class PropertyService {
     public void addProperty(Property property){
         propertyRepository.save(property);
     }
+
+    //Update
+    public void updateProperty(Integer id,Property property){
+        Property oldProperty = propertyRepository.findPropertyById(id);
+        if(oldProperty == null){
+            throw new ApiException("Property not found");
+        }
+        oldProperty.setDescription(property.getDescription());
+        oldProperty.setTitle(property.getTitle());
+        //oldProperty.setIsApproved();
+        propertyRepository.save(oldProperty);
+    }
+
+    //DELETE
+    public void deleteProperty(Integer id){
+        Property delProperty = propertyRepository.findPropertyById(id);
+        if(delProperty == null){
+            throw new ApiException("Property not found");
+        }
+        propertyRepository.delete(delProperty);
+    }
+
 }
