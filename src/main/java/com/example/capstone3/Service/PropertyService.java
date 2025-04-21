f216b582a214b9900981be84ba355939901c2229
AbdullahAli1999 <iifamxx@gmail.com>
2025-04-21 19:43:58 +0300

    i did service for Guarantee and i crate addWith and Assign to

--- src/main/java/com/example/capstone3/Service/PropertyService.java
+++ src/main/java/com/example/capstone3/Service/PropertyService.java
@@ -1,4 +1,23 @@
 package com.example.capstone3.Service;
 
+import com.example.capstone3.Model.Property;
+import com.example.capstone3.Repository.PropertyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class PropertyService {
+    private final PropertyRepository propertyRepository;
+
+    //GET
+    public List<Property> getAllProperty(){
+        return propertyRepository.findAll();
+    }
+    //ADD
+    public void addProperty(Property property){
+        propertyRepository.save(property);
+    }
 }
